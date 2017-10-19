8f0a652a88883c6e87ad7bce4ccce5d83d1d0824
olihel <superpixel@gmail.com>
2017-10-19 14:48:21 +0200

    add basic tests that run on Sauce Labs

---
+++ karma.conf.js
@@ -0,0 +1,97 @@
+getSauceLabsCustomLaunchers = require('./lib/get-sauce-labs-custom-launchers');
+
+const customLaunchers = getSauceLabsCustomLaunchers();
+
+module.exports = function(config) {
+  config.set({
+
+    // base path that will be used to resolve all patterns (eg. files, exclude)
+    basePath: '',
+
+
+    // frameworks to use
+    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+    frameworks: ['jasmine'],
+
+
+    // list of files / patterns to load in the browser
+    files: [
+      { pattern: 'test/test.js', watched: false }
+    ],
+
+
+    // list of files to exclude
+    exclude: [
+    ],
+
+
+    // preprocess matching files before serving them to the browser
+    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+    preprocessors: {
+      'test/test.js': ['webpack']
+    },
+
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress'
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    reporters: ['progress', 'saucelabs'],
+
+
+    // web server port
+    port: 9876,
+
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: true,
+
+
+    // start these browsers
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    browsers: Object.keys(customLaunchers),
+
+
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
+    singleRun: true,
+
+    // Concurrency level
+    // how many browser should be started simultaneous
+    concurrency: 1,
+
+    webpack: {
+      module: {
+        loaders: [
+          {
+            test: /\.(js?|es?)$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader'
+          }
+        ],
+      },
+      devtool: 'source-map',
+      watch: false,
+    },
+
+    sauceLabs: {
+      testName: 'Shadow DOM Karma tests'
+    },
+
+    customLaunchers: customLaunchers,
+
+    // to avoid DISCONNECTED messages
+    browserDisconnectTimeout : 10000, // default 2000
+    browserDisconnectTolerance : 1, // default 0
+    browserNoActivityTimeout : 4 * 60 * 1000, //default 10000
+    captureTimeout : 4 * 60 * 1000 //default 60000
+  })
+}
---
+++ test/test.js
@@ -0,0 +1,50 @@
+const browserSupportsShadowDOM = ('attachShadow' in document.createElement('div'));
+
+import ShadowDOM from '../index.es';
+
+const regularDOM = document.createElement('div');
+const shadowDOMContainer = document.createElement('div');
+const bodyElement = document.querySelector('body');
+
+bodyElement.appendChild(regularDOM);
+bodyElement.appendChild(shadowDOMContainer);
+
+regularDOM.innerHTML = `
+    <style>
+        h3 {
+            color: #f00;
+        }
+    </style>
+    <h3>Regular DOM</span>
+`;
+
+const shadowDOM = new ShadowDOM(shadowDOMContainer);
+
+shadowDOM.innerHTML = `
+    <style>
+        h3 {
+            color: #0f0;
+        }
+    </style>
+    <h3>Shadow DOM</span>
+`;
+
+describe('Basic tests', function() {
+    it('ShadowDOM ponyfill class should exist', function() {
+        expect(ShadowDOM).toBeDefined();
+    });
+
+    it('ShadowDOM instance has a shadowRoot property', function() {
+        expect(shadowDOM.shadowRoot).toBeDefined();
+    });
+
+    if (browserSupportsShadowDOM) {
+        it('shadowRoot property is a ShadowRoot instance if browser supports native shadow DOM', function() {
+            expect(shadowDOM.shadowRoot.toString()).toBe('[object ShadowRoot]');
+        });
+    } else {
+        it('shadowRoot property is an body element instance if browser does not support native shadow DOM', function() {
+            expect(shadowDOM.shadowRoot.toString()).toBe('[object HTMLBodyElement]');
+        });
+    }
+});
