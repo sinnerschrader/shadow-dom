530caeb5e384380257a28f6f5568e3b4449aa9b8
Mario Nebl <hello@mario-nebl.de>
2017-11-29 22:55:53 +0100

    test: switch to browserstack

--- karma.conf.js
+++ karma.conf.js
@@ -1,8 +1,9 @@
-getSauceLabsCustomLaunchers = require('./lib/get-sauce-labs-custom-launchers');
+module.exports = config => {
+  if (!process.env.BROWSER_STACK_USERNAME || !process.env.BROWSER_STACK_ACCESS_KEY) {
+    console.log('Make sure the BROWSER_STACK_USERNAME and BROWSER_STACK_ACCESS_KEY environment variables are set.')
+    process.exit(1)
+  }
 
-const customLaunchers = getSauceLabsCustomLaunchers();
-
-module.exports = function(config) {
   config.set({
 
     // base path that will be used to resolve all patterns (eg. files, exclude)
@@ -35,7 +36,7 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress', 'saucelabs'],
+    reporters: ['dots', 'BrowserStack'],
 
 
     // web server port
@@ -54,19 +55,13 @@ module.exports = function(config) {
     // enable / disable watching file and executing tests whenever any file changes
     autoWatch: true,
 
-
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: Object.keys(customLaunchers),
-
-
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
     singleRun: true,
 
     // Concurrency level
     // how many browser should be started simultaneous
-    concurrency: 1,
+    concurrency: 5,
 
     webpack: {
       module: {
@@ -82,11 +77,50 @@ module.exports = function(config) {
       watch: false,
     },
 
-    sauceLabs: {
-      testName: 'Shadow DOM Karma tests'
+    browserStack: {
+      username: 'hello+browserstack@mario-nebl.de',
+      accessKey: 'qEHVQQt3RmXyZQpBMYaz'
     },
 
-    customLaunchers: customLaunchers,
+    customLaunchers: {
+      bs_chrome: {
+        base: 'BrowserStack',
+        browser: 'chrome',
+        browser_version: '62.0',
+        os: 'OS X',
+        os_version: 'Sierra'
+      },
+      bs_firefox: {
+        base: 'BrowserStack',
+        browser: 'firefox',
+        browser_version: '57.0',
+        os: 'OS X',
+        os_version: 'Sierra'
+      },
+      bs_safari: {
+        base: 'BrowserStack',
+        browser: 'safari',
+        browser_version: '11',
+        os: 'OS X',
+        os_version: 'High Sierra'
+      },
+      bs_ie: {
+        base: 'BrowserStack',
+        browser: 'ie',
+        browser_version: '11.0',
+        os: 'Windows',
+        os_version: '10'
+      },
+      bs_edge: {
+        base: 'BrowserStack',
+        browser: 'edge',
+        browser_version: '16',
+        os: 'Windows',
+        os_version: '10'
+      }
+    },
+
+    browsers: ['bs_chrome', 'bs_firefox', 'bs_safari', 'bs_ie', 'bs_edge'],
 
     // to avoid DISCONNECTED messages
     browserDisconnectTimeout : 10000, // default 2000
