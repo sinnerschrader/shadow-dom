47d75e87c72dfb74d907f0e3f9f365030ce1a1ed
Mario Nebl <hello@mario-nebl.de>
2017-11-30 16:10:50 +0100

    refactor: bootstrap with new strategy

--- src.js
+++ src.js
@@ -1,80 +1,17 @@
-/**
- */
+const SUPPORTS_SHADOW_DOM = ('attachShadow' in HTMLElement.prototype);
 
-/**
- * Flag to determinie native shadowDOM support.
- * Called once on a dummy element.
- * @type {boolean}
- */
-const HAS_SHADOW_DOM = ('attachShadow' in document.createElement('div'))
+export default shadowDom;
 
-/**
- * Ponyfill for attachShadow on `Element`
- * @param {Element} element
- * @param {object} options
- * @returns {Element|false}
- */
-const attachShadow = (element, options) => {
-  // We might be able to do something with options
-  // for now it just exists to replicate the API
-  const iframe = document.createElement('iframe')
-  iframe.setAttribute('frameBorder', 0)
-  element.appendChild(iframe)
-  const {contentDocument} = iframe
-  if (contentDocument && typeof contentDocument === 'object') {
-    const doc = contentDocument.document || contentDocument;
-    doc.open()
-    doc.close()
-    return doc.body
-  }
-  return false
-}
-
-class ShadowDOM {
-  /**
-   * Construct a ponyfill
-   * @param {Element} element
-   * @returns {self}
-   */
-  constructor(element) {
-    this.element = element
-    this._attachShadow()
+function shadowDom(el) {
+  if (SUPPORTS_SHADOW_DOM) {
+    el.attachShadow({mode: 'open'});
+    return el;
   }
 
-  /**
-   * Attaches a native shadowDOM or an iframe
-   * @private
-   */
-  _attachShadow() {
-    if (HAS_SHADOW_DOM) {
-      this.shadowRoot = this.element.attachShadow({mode: 'open'})
-    } else {
-      this.shadowRoot = attachShadow(this.element, {mode: 'open'})
+  return {
+    get shadowRoot() {
+      return el;
     }
-  }
-
-  /**
-   * Ponyfill setter for innerHTML
-   * @param {string} innerHTML
-   */
-  set innerHTML(innerHTML) {
-    this.shadowRoot.innerHTML = innerHTML
-  }
-  /**
-   * Ponyfill getter for innerHTML
-   * @returns {string}
-   */
-  get innerHTML() {
-    return this.shadowRoot.innerHTML
-  }
-
-  /**
-   * Ponyfill method for appendChild
-   * @param {Element} element
-   */
-  appendChild(element) {
-    this.shadowRoot.appendChild(element)
-  }
+  };
 }
 
-export default ShadowDOM
--- test.js
+++ test.js
@@ -1,32 +1,75 @@
-import ShadowDOM from './src';
+import shadowDom from './src';
 
 const HAS_SHADOWDOM = ('attachShadow' in document.createElement('div'));
 
 it('should exist', () => {
-    expect(ShadowDOM).toBeDefined();
+    expect(shadowDom).toBeDefined();
 });
 
 it('has a shadowRoot property', () => {
-  const {shadowDOM, cleanup} = setup();
-  expect(shadowDOM.shadowRoot).toBeDefined();
+  const {scope, cleanup} = setup();
+  expect(scope.shadowRoot).toBeDefined();
   cleanup();
 });
 
-it('is native if supported and polyfilled if not', () => {
-  const {shadowDOM, cleanup} = setup();
-  const expected = HAS_SHADOWDOM ? '[object ShadowRoot]' : '[object HTMLBodyElement]';
-  expect(shadowDOM.shadowRoot.toString()).toBe(expected);+
+it('is native if supported', () => {
+  const {scope, cleanup} = setup();
+  if (HAS_SHADOWDOM) {
+    expect(scope.shadowRoot.toString()).toBe('[object ShadowRoot]');
+  }
   cleanup();
 });
 
-function setup() {
+it('is plain element if unsupported', () => {
+  const {scope, cleanup} = setup();
+  if (!HAS_SHADOWDOM) {
+    expect(scope.shadowRoot instanceof HTMLElement).toBeTruthy();
+  }
+  cleanup();
+});
+
+it('enforces basic scoping', () => {
+  if (HAS_SHADOWDOM) {
+    return;
+  }
+
+  const {scope, cleanup} = fixture('basic-scope');
+  const b = scope.shadowRoot.querySelector('.b');
+  const color = window.getComputedStyle(b).getPropertyValue('color');
+  expect(color).toBe('rgb(0, 128, 0)');
+  cleanup();
+});
+
+function fixture(name) {
+  const html = require(`./fixtures/${name}.html`);
+  return setup(html);
+}
+
+function setup(html) {
   const el = document.createElement('div');
+
+  if (html) {
+    el.innerHTML = html;
+  }
+
   document.body.appendChild(el);
+
+  const provided = el.querySelector('.shadow-dom');
+
+  if (html && !provided) {
+    throw new Error(`setup failed, html provided but no .shadow-dom element found`);
+  }
+
+  const shadowEl = html ? provided : el;
+  const cleanup = () => document.body.removeChild(el);
+  const innerHTML = shadowEl.innerHTML;
+
+  const scope = shadowDom(el);
+  scope.innerHTML = innerHTML;
+
   return {
     el,
-    cleanup() {
-      document.body.removeChild(el);
-    },
-    shadowDOM: new ShadowDOM(el)
+    cleanup,
+    scope
   };
 }
