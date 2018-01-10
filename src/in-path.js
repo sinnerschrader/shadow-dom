0532c4eb2cc0101d5b043447d36d33f9ff51bfb8
Mario Nebl <hello@mario-nebl.de>
2018-01-10 15:53:32 +0100

    test: cover missing base case

--- src/in-path.js
+++ src/in-path.js
@@ -2,4 +2,8 @@ export function inPath(path, base) {
   if (!Array.isArray(path)) {
     throw new TypeError(`path must be array of integers, received ${path}`);
   }
+
+  if (!Array.isArray(base)) {
+    throw new TypeError(`base must be array of integers, received ${base}`);
+  }
 }
--- src/in-path.test.js
+++ src/in-path.test.js
@@ -1,6 +1,9 @@
 import {inPath} from './in-path';
 
-it('returns target', () => {
+it('throws for missing path', () => {
   expect(() => inPath()).toThrowError(/path must be array of integers/);
 });
 
+it('throws for missing base', () => {
+  expect(() => inPath([])).toThrowError(/base must be array of integers/);
+});
