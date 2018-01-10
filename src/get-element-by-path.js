7680b6f0d5f0bbc8f9732e0d7c89c8ce8c948b59
Mario Nebl <hello@mario-nebl.de>
2018-01-10 13:52:41 +0100

    test: cover missing base parameter

--- src/get-element-by-path.js
+++ src/get-element-by-path.js
@@ -5,6 +5,10 @@ export function getElementByPath(path, base) {
     throw new TypeError(`path must be array of positive integers, received ${path}`);
   }
 
+  if (!base) {
+    throw new TypeError(`base must be instance of Node, received ${base}`);
+  }
+
   return path.reduce((el, index) => {
     // TODO: Verify this is no unduly drag on performance
     const childElements = List.filter(el.childNodes, n => n.nodeType === Node.ELEMENT_NODE);
--- src/get-element-by-path.test.js
+++ src/get-element-by-path.test.js
@@ -1,5 +1,10 @@
 import {getElementByPath} from './get-element-by-path';
+import {dom} from './utils.test';
 
 it('throws for missing path', () => {
   expect(() => getElementByPath()).toThrowError(/path must be array of positive integers/);
+});
+
+it('throws for missing base', () => {
+  expect(() => getElementByPath([])).toThrowError(/base must be instance of Node/);
 });
