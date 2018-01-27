6ce26263a45e5eeb3d3013c05d62d6a6762e19ed
Mario Nebl <hello@mario-nebl.de>
2018-01-27 15:29:21 +0100

    test: use dom helper

--- src/element-matches.test.js
+++ src/element-matches.test.js
@@ -1,23 +1,21 @@
 import {elementMatches} from './element-matches';
+import {dom} from './utils.test';
 
 it('throws for missing node', () => {
   expect(() => elementMatches()).toThrowError(/ node must be instance of Node/);
 });
 
 it('throws for missing selector', () => {
-  const parser = new DOMParser();
-  const doc = parser.parseFromString('<html><body></body></html>', 'text/html');
+  const doc = dom('<html><body></body></html>', 'text/html');
   expect(() => elementMatches(doc.body)).toThrowError(/selector must be string/);
 });
 
 it('matches as expected', () => {
-  const parser = new DOMParser();
-  const doc = parser.parseFromString('<html><body></body></html>', 'text/html');
+  const doc = dom('<html><body></body></html>', 'text/html');
   expect(elementMatches(doc.body, 'body')).toBe(true);
 });
 
 it('does not match as expected', () => {
-  const parser = new DOMParser();
-  const doc = parser.parseFromString('<html><body></body></html>', 'text/html');
+  const doc = dom('<html><body></body></html>', 'text/html');
   expect(elementMatches(doc.body, '.foobar')).toBe(false);
 });
